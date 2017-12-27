382d24209b205fde0089f8c8b4f7f52b45b1f45f
mussaddiqsiddique@yahoo.com <mussaddiqsiddique@yahoo.com>
2017-12-27 16:14:59 +0530

    added name property

---
+++ WebApplication5/Models/employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class employee
+    {
+        public string name { get; set; }
+    }
+}
